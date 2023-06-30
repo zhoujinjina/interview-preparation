ce50fac2b33d52eb0d224f12114ebd1897de6a4f
Jinjin <3513776811@qq.com>
2023-06-30 22:26:16 +0800

    update

--- test/arr.js
+++ test/arr.js
@@ -1,9 +1,2 @@
-let arr="4.0.0.2"
-let newArr=arr.replace(/\./g,'')
-console.log(newArr) 
-// .：匹配任意单个字符（除了换行符）
-// 匹配 a、b 或 c
-// 匹配除了 a、b、c 以外的任意单个字符
-//匹配任意小写字母
-// 匹配任意大写字母
-// 匹配任意数字
+let a={a:1,b:'a'}
+console.log(Object.prototype.toString.call(a))
