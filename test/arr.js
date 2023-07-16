980b684f66c41e6c8b93f05dac27b374f783fad6
zhoujinjina <3513776811@qq.com>
2023-07-16 19:02:38 +0800

    update

--- test/arr.js
+++ test/arr.js
@@ -1,15 +1,124 @@
-const quickSort=(arr,n=0)=>{
-    if(arr.length==0) return arr
-    let ns=arr[n]
-    let newArr=arr.splice(n,1)
-    let left=[],right=[]
-    for(let i of arr){
-       if(i>ns){
-        right.push(i)
-       }else{
-        left.push(i)
-       }
+const longestString = (str) => {
+str=str.split('')
+  let left, right;
+  let maxLength = [];
+  let now = [];
+  for (let i = 0; i < str.length; i++) {
+    now = [];
+    right = i+1 ;
+    left = i - 1;
+    now.push(str[i]);
+    while (left >= 0 && right < str.length) {
+      if (str[left] == str[right]) {
+        now.push(str[left]);
+        now.unshift(str[right]);
+        left--;
+        right++;
+      } else{
+        break
+      }
     }
-    return [...quickSort(left),ns,...quickSort(right)]
-}
-console.log(quickSort([1,3,5,3,7,8,4,-1,-5,3,6,5]))
+    if (now.length > maxLength.length) {
+        maxLength = now;
+      }
+  }
+  for (let i = 0; i < str.length; i++) {
+    now = [];
+    right = i;
+    left = i - 1;
+    while (left >= 0 && right < str.length) {
+      if (str[left] == str[right]) {
+        now.push(str[left]);
+        now.unshift(str[right]);
+        left--;
+        right++;
+      } else{
+        break
+      }
+    }
+    if (now.length > maxLength.length) {
+        maxLength = now;
+      }
+  }
+  return maxLength;
+};
+console.log(longestString("sjsajdlkadksasdfjlkasdffjjjjjjda"));
+
+function Sum(...args){
+    // 完善这个函数，实现 Sum 函数链式调用计算多数之和，可通过 sum()().value() 获取计算结果。
+ let value = args.reduce((a, b) => a + b, 0);
+     
+     function sumFunc(...args) {
+       value += args.reduce((a, b) => a + b, 0);
+       return sumFunc;
+     }
+     
+     sumFunc.value = function() {
+       return value;
+     };
+     
+     return sumFunc;
+ }
+console.log(  Sum(1)(2)(3, 4).value())
+console.log(  Sum(1)(2)(3)(4)(5).value())
+console.log( Sum(1,2)(3,4).value())
+class Event {
+    constructor() {
+      this.listeners = new Map();
+    }
+  
+    addEventListener(eventName, listener) {
+      if (!this.listeners.has(eventName)) {
+        this.listeners.set(eventName, []);
+      }
+      this.listeners.get(eventName).push(listener);
+    }
+  
+    removeEventListener(eventName, listener) {
+      if (this.listeners.has(eventName)) {
+        const listeners = this.listeners.get(eventName);
+        const index = listeners.indexOf(listener);
+        if (index !== -1) {
+          listeners.splice(index, 1);
+        }
+      }
+    }
+  
+    addEventListenerOnce(eventName, listener) {
+      const once = (...args) => {
+        this.removeEventListener(eventName, once);
+        listener(...args);
+      };
+      this.addEventListener(eventName, once);
+    }
+  
+    removeAll(eventName) {
+      this.listeners.delete(eventName);
+    }
+  
+    emit(eventName, ...args) {
+      if (this.listeners.has(eventName)) {
+        const listeners = this.listeners.get(eventName);
+        listeners.forEach(listener => listener(...args));
+      }
+    }
+  }
+const event = new Event();
+
+const foo = () => console.log('foo');
+const bar = () => console.log('bar');
+const baz = () => console.log('baz');
+
+event.addEventListener('test', foo);
+event.addEventListener('test', bar);
+event.addEventListener('test', baz);
+
+event.emit('test'); // 'foo', 'bar', 'baz'
+
+event.removeEventListener('test', bar);
+
+event.emit('test'); // 'foo', 'baz'
+
+event.removeAll('test');
+
+event.emit('test'); // No output
