e92a19dc14c5ab86563394481803182f67925ef3
zhoujinjina <3513776811@qq.com>
2023-08-12 21:07:29 +0800

    update

--- a.js
+++ a.js
@@ -2,3 +2,9 @@ let a=[1,3,42,3,3,2,[32,3,23,[32,33,[32,3,[3]]]]]
 console.log(a.toString())
 let b=a.toString().split(",")
 console.log(b)
+let c=[1,2,3,4,5,6]
+let d=c.forEach(item=>{
+   throw new Error
+   console.log('111')
+})
+console.log(d)
