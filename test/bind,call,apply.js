e869858520eafa6bc9bbab63f8923a8ca4ee2c1e
zhoujinjina <3513776811@qq.com>
2023-08-14 17:20:31 +0800

    update

--- test/bind,call,apply.js
+++ test/bind,call,apply.js
@@ -3,28 +3,21 @@ console.log(a.toString())
 let b=a.toString().split(",")
 console.log(b)
 let c=[1,2,3,4,5,6]
-try {
-    var d=c.map(item=>{
-        if(item==4){
-            throw new Error
-        }
-        return item
-     }
-     
-     )
-     
-  
-} catch (error) {
-    console.log(error)
-}
-console.log(d)
-function test(name,age){
-
-}
 // bind apply call 都是构造函数调用 然后调用的时候this指向参数
+// bind函数是生成一个新的函数this指向参数
+// call函数是
 function Person(name,age){
     console.log(this.name)
     console.log(this.age)
     this.name=name
     this.age=age
+    return {}
 }
+let person={name: "John", age:19}
+const newPerson=Person.bind(person)
+newPerson()
+let as=Person.call(person,12,13)
+console.log(person)
+console.log(as)
+let aa=new Person("1",1)
+console.log(aa)
