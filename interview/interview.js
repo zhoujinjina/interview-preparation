1d844eda2744a52cf3bd8bf9d4a45d18d756b2df
zhoujinjina <3513776811@qq.com>
2023-08-16 17:33:20 +0800

    update

---
+++ interview/interview.js
@@ -0,0 +1,35 @@
+function filter(str) {
+  const map = new Map();
+  for (let i of str) {
+    if (map.has(i)) {
+      map.set(i, map.get(i) + 1);
+    } else {
+      map.set(i, 1);
+    }
+  }
+
+  const arr = [...new Set(str)];
+  console.log(map);
+  console.log(arr);
+  let n;//最小
+  let min = map.get(arr[0]);
+  for (let i of arr) {
+    if (map.get(i) < min) {
+      min = map.get(i);
+      n = i;
+    } else if (map.get(i) == min) {
+      n += i;
+    }
+  }
+  const newArr = Array.from(str).map((item) => {
+    if (![...n].includes(item)) {
+      return item;
+    }
+  });
+  return newArr.join("");
+}
+console.log(filter("aaabbbccteeff156546"));
+let fh = [1, 22, 3];
+console.log(fh.indexOf(11));
+
+console.log( typeof Function);
