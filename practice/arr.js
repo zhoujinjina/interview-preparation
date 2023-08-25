cfa0659ddf80249b0f2d0f877b72e4b491f26c05
zhoujinjina <3513776811@qq.com>
2023-08-25 22:31:06 +0800

    update

--- practice/arr.js
+++ practice/arr.js
@@ -22,24 +22,6 @@ str=str.split('')
         maxLength = now;
       }
   }
-  for (let i = 0; i < str.length; i++) {
-    now = [];
-    right = i;
-    left = i - 1;
-    while (left >= 0 && right < str.length) {
-      if (str[left] == str[right]) {
-        now.push(str[left]);
-        now.unshift(str[right]);
-        left--;
-        right++;
-      } else{
-        break
-      }
-    }
-    if (now.length > maxLength.length) {
-        maxLength = now;
-      }
-  }
   return maxLength;
 };
 console.log(longestString("sjsajdlkadjjjda"));
