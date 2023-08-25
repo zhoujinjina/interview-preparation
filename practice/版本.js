fdda2719c74ad4a1040463e722a3f06690fd16ad
zhoujinjina <3513776811@qq.com>
2023-08-25 20:19:18 +0800

    update

--- practice/版本.js
+++ practice/版本.js
@@ -1,3 +1,16 @@
-function mysort(arr){
-      
-}
+function maxSum(arr) {
+    let sum = 0,
+      now = 0;
+    for (let i of arr) {
+      if (now < 0) {
+        now = 0;
+      }
+      now += i;
+      if (now > sum) {
+        sum = now;
+      }
+    }
+    return sum;
+  }
+  // 最大和
+  
