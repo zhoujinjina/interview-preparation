cf2813f8270e1eae05daae75daafb79be7c9dc21
zhoujinjina <3513776811@qq.com>
2023-09-06 17:54:32 +0800

    更新深拷贝

--- interview/interview.js
+++ interview/interview.js
@@ -1,4 +1,24 @@
-function obj(){
-
+function myDeepClone(obj, map = new WeakMap()) {
+  if (obj == null || typeof obj !== "object") {
+    return obj;
+  }
+  if (map.has(obj)) {
+    return map.get(obj);
+  }
+  let newObj = Array.isArray(obj) ? [] : {};
+  map.set(obj, newObj);
+  for (let i in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, i)) {
+      newObj[i] = myDeepClone(obj[i], map);
+    }
+  }
+  return newObj;
 }
-console.log(obj.prototype)
+let tom = { name: "tome" };
+tom.__proto__.age = 12;
+
+let newTom = myDeepClone(tom);
+console.log(newTom);
+tom.name = "tom2";
+console.log(newTom);
+console.log(newTom.__proto__==Object.prototype);
--- preparation/手写题/深拷贝.js
+++ preparation/手写题/深拷贝.js
@@ -1,38 +1,3 @@
-function deepClone(obj) {
-  if (obj === null || typeof obj !== "object") {
-    return obj;
-  }
-  const clonedObj = Array.isArray(obj) ? [] : {};
-
-  for (let key in obj) {
-    clonedObj[key] =obj[key]
-  }
-  return clonedObj;
-}
-let person = {
-  name: "John",
-  age: "16",
-};
-let clone = deepClone(person);
-clone.name = "jbs";
-console.log(clone);
-console.log(person);
-
-
-const deepC=(obj)=>{
-if(obj==null||typeof obj!="object")return obj
-  let cloneObj=Array.isArray(obj)?[] : {};
-  for(let i in obj) {
-    cloneObj[i] = deepClone(obj[i]);
-  }
-
-  return cloneObj;
-
-}
-
-let arr=[1,2,3,4,44,[3,4,4,5]]
-let newArr=deepC(arr)
-console.log(newArr)
 function deepClone(obj, weakMap = new WeakMap()) {  
   if (obj == null || typeof obj !== "object") {  
     return obj;  
