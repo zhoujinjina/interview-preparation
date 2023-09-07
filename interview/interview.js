bbc7b3a06ecae808cb63d92f237dd9c0f522057b
zhoujinjina <3513776811@qq.com>
2023-09-07 21:02:36 +0800

    发布订阅模式

--- interview/interview.js
+++ interview/interview.js
@@ -1,24 +1,2 @@
-function myDeepClone(obj, map = new WeakMap()) {
-  if (obj == null || typeof obj !== "object") {
-    return obj;
-  }
-  if (map.has(obj)) {
-    return map.get(obj);
-  }
-  let newObj = Array.isArray(obj) ? [] : {};
-  map.set(obj, newObj);
-  for (let i in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, i)) {
-      newObj[i] = myDeepClone(obj[i], map);
-    }
-  }
-  return newObj;
-}
-let tom = { name: "tome" };
-tom.__proto__.age = 12;
-
-let newTom = myDeepClone(tom);
-console.log(newTom);
-tom.name = "tom2";
-console.log(newTom);
-console.log(newTom.__proto__==Object.prototype);
+let  set=new Set([1,2,3,4])
+console.log(set)
