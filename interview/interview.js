32c68d67eab1788f5857b016e89e8c490b040ddc
zhoujinjina <3513776811@qq.com>
2023-09-17 12:19:26 +0800

    update

--- interview/interview.js
+++ interview/interview.js
@@ -1,6 +1,7 @@
-
-
 let array = [1, 2, 3, 2, 1, 4, 5, 4];  
+
 let uniqueArray = array.filter((value, index, self) => {  
     return self.indexOf(value) === index;  
-});  
+})
+const newArr = array.reduce((acc, value) => acc.includes(value)  ? acc: [...acc,value], [2]);
+console.log(newArr)
