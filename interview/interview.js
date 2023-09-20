41c03d6becc8fd9f1a567cb7c1d9461f1a5c6fcc
zhoujinjina <3513776811@qq.com>
2023-09-20 22:26:35 +0800

    update

--- interview/interview.js
+++ interview/interview.js
@@ -13,3 +13,32 @@ let a=Symbol(2)
 let b=Symbol(2)
 console.log(a)
 console.log(b==a)
+// actions.js  
+export const fetchPosts = () => ({ type: 'FETCH_POSTS' });  
+  
+// sagas.js  
+import { call, put } from 'redux-saga/effects';  
+import Api from '...';  
+  
+function* fetchPostsWorker(action) {  
+  const response = yield call(Api.fetchPosts);  
+  yield put({ type: 'FETCH_POSTS_SUCCESS', payload: response });  
+}  
+  
+function* watchFetchPosts() {  
+  yield takeEvery('FETCH_POSTS', fetchPostsWorker);  
+}  
+  
+export default function* rootSaga() {  
+  yield all([watchFetchPosts()]);  
+}  
+  
+// store.js  
+import { createStore, applyMiddleware } from 'redux';  
+import createSagaMiddleware from 'redux-saga';  
+import rootReducer from './reducers';  
+import rootSaga from './sagas';  
+  
+const sagaMiddleware = createSagaMiddleware();  
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));  
+sagaMiddleware.run(rootSaga);
