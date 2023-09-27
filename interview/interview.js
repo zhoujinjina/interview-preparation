14466937fad028cfdb0c3aaf3df84e1d84af8684
zhoujinjina <3513776811@qq.com>
2023-09-27 20:56:57 +0800

    Update

--- interview/interview.js
+++ interview/interview.js
@@ -1,11 +1,31 @@
-function maxSubArray(nums) {  
-    let maxSum = nums[0];  
-    let curSum = nums[0];  
+function findKthLargest(nums, k) {  
+    let arr = nums.slice();  
+    let left = 0;  
+    let right = arr.length - 1;  
       
-    for (let i = 1; i < nums.length; i++) {  
-      curSum = Math.max(nums[i], curSum + nums[i]);  
-      maxSum = Math.max(maxSum, curSum);  
+    while (left <= right) {  
+      let pivotIndex = partition(arr, left, right);  
+      if (pivotIndex === k - 1) {  
+        return arr[pivotIndex];  
+      } else if (pivotIndex < k - 1) {  
+        left = pivotIndex + 1;  
+      } else {  
+        right = pivotIndex - 1;  
+      }  
     }  
       
-    return maxSum;  
+    return null;  
+  }  
+    
+  function partition(arr, left, right) {  
+    let pivot = arr[right];  
+    let i = left;  
+    for (let j = left; j < right; j++) {  
+      if (arr[j] <= pivot) {  
+        [arr[i], arr[j]] = [arr[j], arr[i]];  
+        i++;  
+      }  
+    }  
+    [arr[i], arr[right]] = [arr[right], arr[i]];  
+    return i;  
   }
